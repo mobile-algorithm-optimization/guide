052c750001d013266fb80f7f8c2fede6bff28b36
mobile-algorithm-optimization <94515058+mobile-algorithm-optimization@users.noreply.github.com>
2021-12-29 13:05:00 +0800

    Revert "Open cl transpose (#1)" (#2)
    This reverts commit dc41d566eca697e78d9034e053d263a5c9629e04.

--- mtk_test.sh
+++
@@ -1,6 +0,0 @@
-cd build_android
-rm -r *
-cmake -DANDROID=android -DMTK=mtk -DCMAKE_BUILD_TYPE=Release ..
-make install
-adb push install/bin/* /data/local/Transpose/bin/
-adb shell < ../mtk_adbshell.sh
