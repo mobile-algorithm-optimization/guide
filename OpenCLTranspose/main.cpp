0a10dcca06dac53a856cf0b6bccf431d6f5282ba
ZhouJieYeah <51687761+ZhouJieYeah@users.noreply.github.com>
2022-10-24 19:31:39 +0800

    fix opencl event leak (#3)
    Co-authored-by: zhoujie6 <zhoujie6@xiaomi.com>

--- OpenCLTranspose/main.cpp
+++ OpenCLTranspose/main.cpp
@@ -133,17 +133,18 @@ int main()
 
     printf("global_work_size=(%zu,%zu)\n", global_work_size[0], global_work_size[1]);
     printf("local_work_size=(%zu,%zu)\n", local_work_size[0], local_work_size[1]);
-    cl_event kernel_event = NULL;
     gettimeofday(&start, NULL);
     for (int i = 0; i < c_loop_count; i++)
     {
+        cl_event kernel_event = NULL;
         err_num = clEnqueueNDRangeKernel(command_queue, kernel, 2, NULL, global_work_size,
                                          local_work_size, 0, NULL, &kernel_event);
         CheckClStatus(err_num, "ClEnqueueNDRangeKernel");
         err_num = clWaitForEvents(1, &kernel_event);
         CheckClStatus(err_num, "ClWaitForEvents");
+        PrintProfilingInfo(kernel_event);
+        clReleaseEvent(kernel_event);
     }
-    //PrintProfilingInfo(kernel_event);
     PrintDuration(&start, "OpenCL Transpose", c_loop_count);
 
     err_num = clEnqueueReadBuffer(command_queue, buffer_dst, CL_TRUE, 0, buffer_size_in_bytes,
@@ -155,7 +156,6 @@ int main()
     free(host_src_matrix);
     free(host_transposed_matrix);
     free(device_transposed_matrix);
-    clReleaseEvent(kernel_event);
     clReleaseMemObject(buffer_src);
     clReleaseMemObject(buffer_dst);
 
